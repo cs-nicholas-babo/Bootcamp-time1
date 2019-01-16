cd6000d717b1e658d8f9e1567dea3fa94f393390
Nicholas Babo <nicholas.r.babo@concrete.com.br>
2019-01-16 15:09:34 -0200

    CacheManager and Set Cache Repository created

---
+++ DesafioBootcamp/PlatformLocalDatabase/Entities/Repository/CacheRepository.swift
@@ -0,0 +1,53 @@
+//
+//  CacheRepository.swift
+//  PlatformLocalDatabase
+//
+//  Created by nicholas.r.babo on 16/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+import Domain
+
+public final class CacheManager{
+    
+    let cardSetRepository: CardSetCacheRepository
+    
+    init (realm:Realm){
+        self.cardSetRepository = CardSetCacheRepository(realm: realm)
+    }
+    
+}
+
+final class CardSetCacheRepository: RealmRepository{
+    typealias T = CardSet
+    
+    let realm: Realm
+    
+    init(realm: Realm){
+        self.realm = realm
+    }
+    
+    func get() -> [CardSet] {
+        return realm.objects(RealmCardSet.self).map({ $0.baseData()})
+    }
+    
+    func upsert(object: CardSet) {
+        let realmObject = object.realmData()
+        
+        try! realm.write {
+            realm.add(realmObject, update: true)
+        }
+    }
+    
+    func delete(object: CardSet) {
+        let realmObject = realm.object(ofType: RealmCardSet.self, forPrimaryKey: object.code)
+        
+        if let realmObject = realmObject{
+            try! realm.write {
+                realm.delete(realmObject)
+            }
+        }
+    }
+}
