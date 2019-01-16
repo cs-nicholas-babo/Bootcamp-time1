85c1978d0916d826a9d469ea3664180d5f3a80f5
Guilherme 'Heavz' Guimarães <cs-g-guimaraes@accenture.com>
2019-01-16 14:04:25 -0200

    base implementation of LocalDatabase entities

---
+++ DesafioBootcamp/PlatformLocalDatabase/Entities/RealmCard.swift
@@ -0,0 +1,21 @@
+//
+//  RealmCard.swift
+//  PlatformLocalDatabase
+//
+//  Created by g.guimaraes on 16/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+public final class RealmCard: RealmSwift.Object {
+    @objc dynamic var id: String = ""
+    @objc dynamic var name: String = ""
+    @objc dynamic var setCode: String = ""
+//    @objc dynamic var types: List<RealmCardType> = List<RealmCardType>()
+    
+    override public static func primaryKey() -> String {
+        return "id"
+    }
+}
