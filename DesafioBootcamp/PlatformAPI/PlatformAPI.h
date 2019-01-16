2bd19ca47f0fa462159bcb2e1673ecf3d4843b0e
Nicholas Babo <nicholas.r.babo@concrete.com.br>
2019-01-16 11:34:19 -0200

    Platform frameworks created

---
+++ DesafioBootcamp/PlatformAPI/PlatformAPI.h
@@ -0,0 +1,19 @@
+//
+//  PlatformAPI.h
+//  PlatformAPI
+//
+//  Created by nicholas.r.babo on 16/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlatformAPI.
+FOUNDATION_EXPORT double PlatformAPIVersionNumber;
+
+//! Project version string for PlatformAPI.
+FOUNDATION_EXPORT const unsigned char PlatformAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatformAPI/PublicHeader.h>
+
+
---
+++ DesafioBootcamp/PlatformLocalDatabase/PlatformLocalDatabase.h
@@ -0,0 +1,19 @@
+//
+//  PlatformLocalDatabase.h
+//  PlatformLocalDatabase
+//
+//  Created by nicholas.r.babo on 16/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlatformLocalDatabase.
+FOUNDATION_EXPORT double PlatformLocalDatabaseVersionNumber;
+
+//! Project version string for PlatformLocalDatabase.
+FOUNDATION_EXPORT const unsigned char PlatformLocalDatabaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlatformLocalDatabase/PublicHeader.h>
+
+
