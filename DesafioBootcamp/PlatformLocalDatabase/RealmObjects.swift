81a298f0acd110ba7e4981c980ff8518e97d8e1b
Nicholas Babo <nicholas.r.babo@concrete.com.br>
2019-01-16 13:29:58 -0200

    Created LocalProject branch

---
+++ DesafioBootcamp/PlatformLocalDatabase/RealmObjects.swift
@@ -0,0 +1,9 @@
+//
+//  RealmObjects.swift
+//  PlatformLocalDatabase
+//
+//  Created by nicholas.r.babo on 16/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+import Foundation
