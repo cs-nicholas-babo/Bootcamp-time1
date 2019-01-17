43ac3e4aa98d7bffc65380c1719188bc38f15ea5
cs-miguel-nery <miguel.horta.nery@concrete.com.br>
2019-01-17 11:35:06 -0200

    Created abstract factory for CardSetList scene + fixed targets errors
    Co-authored-by: Ricardo Rachaus <ricardo.s.rachaus@concrete.com.br>

---
+++ DesafioBootcamp/DesafioBootcamp/Application/Scenes/Commons/Factory.swift
@@ -0,0 +1,13 @@
+//
+//  Factory.swift
+//  DesafioBootcamp
+//
+//  Created by miguel.horta.nery on 17/01/19.
+//  Copyright © 2019 concrete.solutions. All rights reserved.
+//
+
+import UIKit
+
+protocol Factory {
+    static func make() -> UIViewController
+}
