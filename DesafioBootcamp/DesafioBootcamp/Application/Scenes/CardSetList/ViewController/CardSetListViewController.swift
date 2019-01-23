d0014a204b31afdf43a6470023832416a909c2f2
Mik <miguel.horta.nery@concrete.com.br>
2019-01-23 17:34:27 -0200

    fix data load on CardSetList setup

--- DesafioBootcamp/DesafioBootcamp/Application/Scenes/CardSetList/ViewController/CardSetListViewController.swift
+++ DesafioBootcamp/DesafioBootcamp/Application/Scenes/CardSetList/ViewController/CardSetListViewController.swift
@@ -49,6 +49,9 @@ final class CardSetListViewController: UIViewController {
         if !hasLoaded{
             _ = stateMachine.enter(CardSetListLoadingState.self)
             hasLoaded = true
+        } else {
+            guard let interactor = self.interactor else { fatalError() }
+            interactor.fetchSet()
         }
     }
     
